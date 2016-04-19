e9f60af60ff426f80be02812c84eb7a81ac3ddbc
yuchen2012 <yc3@princeton.edu>
2016-04-18 23:58:08 -0400

    structure changes
    modules folder to include costum modules

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./db/db');
+var db = require('../modules/db/db');
 
 router.route('/')
 .get(function(req, res, next) {
