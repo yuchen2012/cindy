7fbb119d5bb0fc056474c839512fb498103864e2
yuchen2012 <yc3@princeton.edu>
2016-04-20 00:01:28 -0400

    a few db functions
    a few db functions

--- modules/db/db.js
+++ modules/db/db.js
@@ -28,37 +28,37 @@ function runSql(sql,data){
 
 
 /*
-User table
+user table
 */
 
-exports.setup = function(){
-	db.run('CREATE TABLE IF NOT EXISTS user(user VARCHAR(255),pwd VARCHAR(255))');
+exports.userSetup = function(){
+	db.run('CREATE TABLE IF NOT EXISTS user(username VARCHAR(255), email VARCHAR(255), password VARCHAR(255), friends TEXT)');
 }
 
 exports.addUser = function(data){
-	sql = "INSERT INTO user (user, pwd) VALUES (?, ?)";
+	sql = 'INSERT INTO user(username, email, password) VALUES (?, ?, ?)';
 	runSql(sql,data);
 }
 
 
 
 exports.deleteUserById =function(RecNo){
-	sql = "DELETOE FROM user WHERE RecNo = ?";
+	sql = 'DELETE FROM user WHERE RecNo = ?';
 	runSql(sql,RecNo);
 }
 
 exports.updateUser = function(data){
-	sql = "UPDATE user SET user = ?, pwd = ? WHERE RecNo =?";
+	sql = 'UPDATE user SET username = ?, email = ?, password = ? WHERE RecNo =?';
 	runSql(sql,data);
 }
 
 exports.loginCheck = function(username,pwd,res){
-	sql = "SELECT * FROM user WHERE user = ?";
+	sql = 'SELECT password FROM user WHERE username = ?';
 	db.all(sql,username,function(err,row){
 		if(err){
 			console.log(err);
 		}else{
-			if(row[0].pwd == pwd) res.redirect('/home');
+			if(row[0].password == pwd) res.redirect('/home');
 			else{
 				res.redirect('login');
 			}
@@ -66,3 +66,48 @@ exports.loginCheck = function(username,pwd,res){
 	});
 }
 
+exports.usernameCheck = function(username,res){
+	
+}
+
+/*
+item table
+*/
+exports.itemSetup = function(){
+	db.run('CREATE TABLE IF NOT EXISTS item(name VARCHAR(255), description TEXT, url TEXT, blueprintId INT)');
+}
+
+exports.addItem = function(data){
+	sql = 'INSERT INTO item(name, description, url, blueprintID) VALUES (?, ?, ?, ?)';
+	runSql(sql,data);
+}
+
+
+
+exports.deleteUserById =function(RecNo){
+	sql = 'DELETE FROM user WHERE RecNo = ?';
+	runSql(sql,RecNo);
+}
+
+exports.updateUser = function(data){
+	sql = 'UPDATE user SET username = ?, email = ?, password = ? WHERE RecNo =?';
+	runSql(sql,data);
+}
+
+/*
+progress table
+*/
+
+/*
+chapter table
+*/
+exports.chapterSetup = function(){
+	db.run('CREATE TABLE IF NOT EXISTS chapter(name VARCHAR(255), txt TEXT)');
+}
+
+/*
+blueprint table
+*/
+exports.blueprintSetup = function(){
+	db.run('CREATE TABLE IF NOT EXISTS blueprint(description TEXT, logo TEXT)');
+}
