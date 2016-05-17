d789b67084eafa9071401c894cf0a2e87e1779d0
yuchen2012 <yc3@princeton.edu>
2016-05-17 01:46:38 -0400

    minor changes
    minor changes

--- routes/management/blueprint.js
+++ routes/management/blueprint.js
@@ -1,50 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../../modules/db/db');
-
+db.connect();
 
 router.route('/')
 .get(function(req,res){
-	db.connect();
+	
 	db.showBlueprint(0,0,res,'management');
-	db.disconnect();
+	
 })
 .post(function(req,res){
-	db.connect();
+
 	db.addBlueprint([req.body.name,req.body.description,req.body.logo]);
-	db.disconnect();
+	
 	res.redirect('/management/blueprint');		
 });
 
 router.route('/show')
 .get(function(req,res){
-	db.connect();
+	
 	db.showBlueprint(0,0,res,'show');
-	db.disconnect();
+	
 });
 
 router.route('/delete/:id').get(function(req,res){
-	db.connect();
+	
 	db.deleteBlueprint(req.params.id);
-	db.disconnect();
+	
 	res.redirect('/management/blueprint');
 });
 
 router.route('/update/:id').get(function(req,res){
-	db.connect();
+	
 	db.lookupBlueprint(req.params.id,res,'management');
-	db.disconnect();
+	
 }).post(function(req,res){
-	db.connect();
+	
 	db.updateBlueprint([req.body.name,req.body.description,req.body.logo,req.body.rowid]);
-	db.disconnect();
+	
 	res.redirect('/management/blueprint');
 });
 
 router.route('/create/:id').get(function(req,res){
-	db.connect();
+	
 	db.addItem([0,req.params.id]);
-	db.disconnect();
+	
 	res.redirect('/management/item');
 });
 
--- routes/management/user.js
+++ routes/management/user.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../../modules/db/db');
-
+db.connect();
 
 router.route('/')
 .get(function(req,res){
-	db.connect();
+	
 	db.showUser(0,0,res,'management');
-	db.disconnect();
+	
 });
 
 router.route('/show')
 .get(function(req,res){
-	db.connect();
+	
 	db.showUser(0,0,res,'show');
-	db.disconnect();
+	
 });
 
 router.route('/delete/:id').get(function(req,res){
-	db.connect();
+	
 	db.deleteUser(req.params.id);
-	db.disconnect();
+	
 	res.redirect('/management/user');
 });
 
